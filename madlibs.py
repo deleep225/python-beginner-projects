975276d819790a8421c23fd79f73fc439f31a04c
deleep225 <43469578+deleep225@users.noreply.github.com>
2024-01-09 07:12:57 -0800

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#string concatenation 
+# youtuber="dhileep"#
+# print("subscrib to"+ youtuber)
+# print("subscrib to{}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+adj=input("adjctive:")
+verb1=input("verb1")
+verb2=input("verb2")
+famous_person=input("famous_person")
+madlib=f"computer programming is so {adj}! it makes me so excited all the time \
+    because I love {verb1}. stay hydrated {verb2} like you are {famous_person}"
+print(madlib)
